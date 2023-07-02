42b51632d41e137824e741382b60b391fb09e30f
matteomira <matteo.mirabilii@gmail.com>
2023-07-02 18:15:38 +0200

    aggiunto esercizio 2

---
+++ M2 - Sistemi operativi e linguaggi/D3. Linguaggio C/esercizio2.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+
+#define MAX_LEN 30
+
+
+
+int main()
+{
+	int punteggio = 0;
+	char sceltamenu, nomeutente[MAX_LEN], rispostap, rispostas;
+
+	printf("Benvenuto/a!\n");
+	printf("Rispondi correttamente alle domande!\n\n");
+
+	do
+	{
+		while (sceltamenu != 'A' && sceltamenu != 'B')
+		{
+			printf("\n\tMENU\n");
+			printf("A - Iniziare una nuova partita\n");
+			printf("B - Uscire dal gioco\n");
+			printf("Digita la lettera per scegliere l'opzione...\n");
+			scanf("%s", &sceltamenu);
+		}
+
+
+		if (sceltamenu == 'A')
+		{
+			printf("\nInserisci il nome utente (max 15 caratteri):  ");
+			scanf("%s", nomeutente);
+
+			fflush(stdin);
+
+			while (rispostap != '1' && rispostap != '2' && rispostap != '3')
+			{
+				printf("\n\tDomanda 1\n");
+				printf("Qual e' la capitale dell'Italia?\n 1 - Canicatti'\n 2 - Roma\n 3 - Ravenna\n");
+				printf("Per rispondere inserisci il numero corrispondente:  ");
+				scanf("%s", &rispostap);
+			}
+
+			if (rispostap == '2')
+			{
+				printf("\nRisposta esatta!\n");
+				punteggio++;
+			}
+			else
+			{
+				printf("\nRisposta sbagliata!\n");
+			}
+
+			while (rispostas != '1' && rispostas != '2' && rispostas != '3')
+			{
+				printf("\n\tDomanda 2\n");
+				printf("Qual e' il fiume piu' lungo d'Italia?\n 1 - Flumendosa\n 2 - Arno\n 3 - Po\n");
+				printf("Per rispondere inserisci il numero corrispondente:  ");
+				scanf("%s", &rispostas);
+			}
+
+			if (rispostas == '3')
+			{
+				printf("\nRisposta esatta!\n");
+				punteggio++;
+			}
+			else
+			{
+				printf("\nRisposta sbagliata!\n");
+			}
+
+			printf("\nIl punteggio totale dell'utente %s e':  %d\n", nomeutente, punteggio);
+		}
+		else
+		{
+			return 0;
+		}
+
+		sceltamenu = '0';
+
+	} while (1);
+
+	return 0;
+}
